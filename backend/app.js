c849eea8ecb30652df6a8a2214237de4da77f8c0
Nicollas Isaac <79726241+nicollasisaac@users.noreply.github.com>
2023-06-15 20:39:16 -0300

    att endpointPesquisa

--- backend/app.js
+++ backend/app.js
@@ -28,13 +28,10 @@ app.use(bodyParser.text({ type: 'text/plain' }));
 // Configuração do servidor para lidar com requisições codificadas em URL
 app.use(bodyParser.urlencoded({ extended: false }));
 
-/* Definição dos endpoints*/
+// endpoint responsável pela pesquisa
 app.get('/pesquisa', (req, res) => {
     res.statusCode = 200;
     res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Abre o banco
-    var db = new sqlite3.Database(DBPATH);
 
     // Separação dos termos de pesquisa em um array
     const termosPesquisa = req.query.termo.trim().split(" ");
@@ -51,69 +48,42 @@ app.get('/pesquisa', (req, res) => {
 
     // Criação de uma string com os termos de pesquisa para a cláusula "WHERE" da consulta SQL
     const termosParams = termosPesquisa
-        .map(() => '(cat_dados_variaveis.campos_variaveis LIKE ? OR cat_dados_tabela.nome_tabela LIKE ? OR cat_dados_tabela.conjunto_de_dados LIKE ? OR cat_dados_tabela.conteudo_tabela LIKE ? OR cat_dados_owner.nome_data_owner LIKE ? COLLATE NOCASE)')
+        .map(() => '(cat_dados_tabela.nome_tabela LIKE ? OR cat_dados_tabela.conjunto_de_dados LIKE ? OR cat_dados_tabela.conteudo_tabela LIKE ? OR cat_dados_owner.nome_data_owner LIKE ? COLLATE NOCASE)')
         .join(' OR ');
 
     let sql = `SELECT 
-    cat_dados_tabela.*, cat_dados_owner.*, cat_dados_conexoes.*, REPLACE(cat_dados_variaveis.campos_variaveis, ',', ' ') as campos_variaveis,
-    cat_dados_variaveis.campo_estrangeiro, feedback.classificacao_admin, feedback.qtd_like_colaborador
-  FROM 
+    cat_dados_tabela.*, cat_dados_owner.*, cat_dados_conexoes.*
+FROM 
     cat_dados_tabela
-    INNER JOIN feedback ON cat_dados_tabela.id_numerico = feedback.id_numerico
     INNER JOIN cat_dados_owner ON cat_dados_tabela.conjunto_de_dados = cat_dados_owner.conjunto_de_dados
-    INNER JOIN cat_dados_conexoes ON cat_dados_tabela.id_tabela = cat_dados_conexoes.id_tabela
-    INNER JOIN (
-      SELECT 
-        cat_dados_variaveis.id_tabela, GROUP_CONCAT(nome_tabela || ' ' || nome_campo || ' ' || tipo_campo || ' ' || tipo_pessoa || ' ' || descricao_campo) as campos_variaveis
-      FROM 
-        cat_dados_variaveis
-      GROUP BY 
-        cat_dados_variaveis.id_tabela      
-    ) AS cat_dados_variaveis ON cat_dados_tabela.id_tabela = cat_dados_variaveis.id_tabela
-    LEFT JOIN (
-      SELECT 
-        cat_dados_variaveis.id_tabela, cat_dados_variaveis.nome_campo AS campo_estrangeiro
-      FROM 
-        cat_dados_tabela 
-        JOIN cat_dados_owner ON cat_dados_tabela.conjunto_de_dados = cat_dados_owner.conjunto_de_dados
-        JOIN cat_dados_conexoes ON cat_dados_tabela.id_tabela = cat_dados_conexoes.id_tabela
-        LEFT JOIN cat_dados_variaveis ON cat_dados_variaveis.id_tabela = cat_dados_tabela.id_tabela
-          AND cat_dados_variaveis.ch_primaria = "S"
-    ) AS cat_dados_variaveis ON cat_dados_tabela.id_tabela = cat_dados_variaveis.id_tabela`;
+    INNER JOIN cat_dados_conexoes ON cat_dados_tabela.id_tabela = cat_dados_conexoes.id_tabela`;
 
     const params = [...termosLike, ...termosLike, ...termosLike, ...termosLike];
 
     sql += ' WHERE ' + termosParams;
 
     if (conjuntoDeDadosFiltro) {
-        sql += ` AND cat_dados_tabela.conjunto_de_dados LIKE '%${conjuntoDeDadosFiltro}%'`;
+        sql += ' AND cat_dados_tabela.conjunto_de_dados = ?';
+        params.push(conjuntoDeDadosFiltro);
     }
 
     if (dadosSensiveisFiltro) {
-        sql += ` AND cat_dados_tabela.dados_sensiveis_tabela LIKE '%${dadosSensiveisFiltro}%'`;
+        sql += ' AND cat_dados_tabela.dados_sensiveis_tabela = ?';
+        params.push(dadosSensiveisFiltro);
     }
 
     if (ownerFiltro) {
-        sql += ` AND cat_dados_owner.nome_data_owner LIKE '%${ownerFiltro}%'`;
+        sql += ' AND cat_dados_owner.nome_data_owner = ?';
+        params.push(ownerFiltro);
     }
 
     if (stewardFiltro) {
-        sql += ` AND cat_dados_steward.nome_data_owner LIKE '%${stewardFiltro}%'`;
+        sql += ' AND cat_dados_steward.nome_data_owner = ?';
+        params.push(stewardFiltro);
     }
-
-    if (ordemFiltro){
-        sql += `
-            ORDER BY 
-              feedback.classificacao_admin_feedback DESC, feedback.qtd_like_colaborador_feedback DESC`;
-    } else{
-        sql += `
-            ORDER BY 
-              feedback.classificacao_admin_feedback ASC, feedback.qtd_like_colaborador_feedback ASC`;
-    }
-
+    
+    var db = new sqlite3.Database(DBPATH); // Abre o banco
     db.all(sql, params, (err, rows) => {
-        // Fecha o banco
-        db.close();
         if (err) {
             console.error(err);
             return res.status(500).send('Erro interno do servidor.');
@@ -121,6 +91,7 @@ app.get('/pesquisa', (req, res) => {
         res.json(rows);
     });
 });
+
 
 app.get('/resultado', (req, res) => {
     res.statusCode = 200;
